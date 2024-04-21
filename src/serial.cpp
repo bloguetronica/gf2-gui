5789cb76f94b4ddf4e471c266fedd68911140e01
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-04-21 17:48:56 +0100

    Add files via upload

--- src/serial.cpp
+++ src/serial.cpp
@@ -1,5 +1,5 @@
-/* GF2 GUI - Version 2.0 for Debian Linux
-   Copyright (c) 2018 Samuel Lourenço
+/* GF2 GUI - Version 2.1 for Debian Linux
+   Copyright (c) 2018-2024 Samuel Lourenço
 
    This program is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the Free
